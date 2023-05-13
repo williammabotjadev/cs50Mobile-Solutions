f503598ebe172df63d108548ff9002d2e298cc79
williammabotjadev <williammabotjadev@gmail.com>
2023-05-13 20:51:39 +0200

    NavBar

--- project2/App.js
+++ project2/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <NavBar />
       <Text>Setup, Ready to Go!</Text>
       <StatusBar style="auto" />
     </View>
